7cfb163f5410bbfceb48af9fbba4347feda16fdd
Nika <example@gmail.com>
2020-08-14 13:22:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Hello, ${user}`);
+
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr); 
+console.log(--decr); 
